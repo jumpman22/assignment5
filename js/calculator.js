d3070cb00edb189447902c4c166c37fd7e4b8a93
jumpman22 <hiram.cho@gmail.com>
2018-03-22 14:58:35 -0700

    finish calculator

--- js/calculator.js
+++ js/calculator.js
@@ -1,2 +1,49 @@
 /*eslint-env browser*/
 
+window.addEventListener("load", init);
+
+function init(){
+	var body = window.document.getElementById("body");
+	body.addEventListener("click", enter);
+}
+//	one.addEventListener("click", enter);
+//	two.addEventListener("click", enter);
+//	three.addEventListener("click", enter);
+//	four.addEventListener("click", enter);
+//	five.addEventListener("click", enter);
+//	six.addEventListener("click", enter);
+//	seven.addEventListener("click", enter);
+//	eight.addEventListener("click", enter);
+//	nine.addEventListener("click", enter);
+//	add.addEventListener("click", enter);
+//	subtract.addEventListener("click", enter);
+//	multiply.addEventListener("click", enter,);
+//	divide.addEventListener("click", enter,);
+//	period.addEventListener("click", enter,);
+//	zero.addEventListener("click", enter);
+		equal.addEventListener("click", calculate);
+
+//var display = window.document.getElementById("result");
+//
+//function enter(val){
+//	display.value += this.value;
+//}
+//
+//function calculate(){
+//	display.value = eval(display.value);
+//}
+
+var display = document.getElementById("result");
+
+function enter(num, val){
+	if (num.target !== num.currentTarget && num.target.value !== "="){
+		var numSelect = num.target.value;
+		display.value += numSelect;
+	} 
+	num.stopPropagation();	
+}
+
+function calculate(){
+	display.value = eval(display.value);
+	}
+
--- js/dom-practice.js
+++ js/dom-practice.js
@@ -27,6 +27,7 @@
 //STEP 5 Modify the code in step 4 so that you can write your script within the <head> tag of the page rather than underneath the button in the markup.
 //window.addEventListener("load", init);
 //function init(){
+//	"use strict";
 //	btn.addEventListener("click", hello);
 // 		function hello(){
 // 		window.alert("I have been clicked!");
@@ -34,30 +35,65 @@
 //};
 //var btn = document.getElementById("hola");
 //STEP 6 Add a link to your web page. Add the text “Visit Google” to the link, set the ID to “redirect” and set the href attribute to http://www.google.com. Now, write a script that alerts the user that they clicked on the link but prevents the browser from also redirecting to the Google site.
-window.addEventListener("load", init);
-function init(){
-	redirect.addEventListener("click", function(event){
-		event.preventDefault();
-		window.alert("You wanted Google but you're not going there!")
-	});
-}
-//STEP 7
+//window.addEventListener("load", init);
+//function init(){
+//	"use strict";
+//	redirect.addEventListener("click", function(event){
+//		event.preventDefault();
+//		window.alert("You wanted Google but you're not going there!")
+//	});
+//}
+//STEP 7 Add a text box and a button to a web page. Add an event listener to the button. When the click event is heard, the button should be disabled, and the text that the user input into the text box should be displayed within an alert box.
+//function displayText() {
+//		"use strict";
+//		btnSubmit.disabled = true;
+//		alert(textInput.value);
+//}
+//var textInput = window.document.getElementById("textInput");
+//var btnSubmit = document.getElementById("submit");
+//btnSubmit.addEventListener("click", displayText);
+//STEP 8 Create a new page called newpage.html. You will now have 2 web pages including practice.html and newpage.html. Add a button to practice.html. When the button is clicked, newpage.html should appear as a popup window at 300 pixels wide by 300 pixels high. As soon as newpage.html loads however, resize the window to 500 pixels wide by 300 pixels high. You will need to handle the load event on newpage.html and use a BOM method on newpage.html to accomplish this task.
+//function newPage(){
+//	"use strict";
+//	var openPage = window.document.getElementById("btnPage");
+//	openPage.addEventListener("click", function(){
+//		window.open("newpage.html", "btnPage", "width=300, height=300")
+//	});
+//}
+//window.addEventListener("load", newPage);
+//STEP 9 Add two buttons to a web page. Add the text Start to one button and Stop to the other button. Using the BOM .setInterval() method, figure out a way to move display the text “Learning JavaScript is fun!” into the console window every 2 seconds but only when the Start button is clicked. If the user clicks on the Stop button, the console should stop logging the text immediately.
+//var msg;
+//function showMsg(){
+//	msg = setInterval(function(){
+//		window.console.log("Learning Javascript is fun!")
+//	}, 2000);
+//}
+//
+//function stop(){
+//	clearInterval(msg);
+//}
+//var startBtn = window.document.getElementById("startBtn");
+//var stopBtn = window.document.getElementById("stopBtn");
+//startBtn.addEventListener("click", showMsg);
+//stopBtn.addEventListener("click", stop);
+//STEP 10 Add a drop down list to a web page. Populate the drop down list with 4 items. Add a button next to the drop down list and set the text to “Select”. When the user clicks the button, the item selected in the drop down list should appear within an alert box.
+//function showTools(){
+//	var toolSelect = tools.value;
+//	window.alert("You chose " + toolSelect);
+//}
+//
+//var selectBtn = window.document.getElementById("select");
+//selectBtn.addEventListener("click", showTools);
 
-//STEP 8
-
-//STEP 9
-
-//STEP 10
 
 
 
-	
-//2.	
-//3.	
-//4.	
-//5.	
-//6.	
-//7.	Add a text box and a button to a web page. Add an event listener to the button. When the click event is heard, the button should be disabled, and the text that the user input into the text box should be displayed within an alert box.
-//8.	Create a new page called newpage.html. You will now have 2 web pages including practice.html and newpage.html. Add a button to practice.html. When the button is clicked, newpage.html should appear as a popup window at 300 pixels wide by 300 pixels high. As soon as newpage.html loads however, resize the window to 500 pixels wide by 300 pixels high. You will need to handle the load event on newpage.html and use a BOM method on newpage.html to accomplish this task.
-//9.	Add two buttons to a web page. Add the text Start to one button and Stop to the other button. Using the BOM .setInterval() method, figure out a way to move display the text “Learning JavaScript is fun!” into the console window every 2 seconds but only when the Start button is clicked. If the user clicks on the Stop button, the console should stop logging the text immediately.
-//10.	Add a drop down list to a web page. Populate the drop down list with 4 items. Add a button next to the drop down list and set the text to “Select”. When the user clicks the button, the item selected in the drop down list should appear within an alert box.
+
+
+
+
+
+
+
+
+
