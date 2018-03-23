72c7dac432ed3396115e6fc315ca0d6fc9ddb68c
jumpman22 <hiram.cho@gmail.com>
2018-03-22 20:58:17 -0700

    del function

--- js/employee-management.js
+++ js/employee-management.js
@@ -1,17 +1,56 @@
-/*eslint-env browser*/
-
-var employees = [
-	{name: "Mike Jones", title: "CEO", extension: "3574"},
+var employees  = [{name: "Mike Jones", title: "CEO", extension: "3574"},
 	{name: "Sally Rogers", title: "Marketing", extension: "5451"},
 	{name: "Paul Handle", title: "Finance", extension: "4847"},
 	{name: "Betty Childs", title: "Human Resources", extension: "3586"},
-	{name: "Colby Lovell", title: "Janitor", extension: "6985"}
-];
+	{name: "Colby Lovell", title: "Janitor", extension: "6985"}];
 
-https://jsfiddle.net/gvanderberg/cwmzyjf4/
-for(var i = 0; i < employees.length; i++) {
-  document.write("<tr><td>Number " + i + " is:</td>");
-  document.write("<td>" + employees[i].name + "</td></tr>");
+var titleInput  = document.getElementById("title");
+var nameInput   = document.getElementById("name");
+var extInput = document.getElementById("extension");
+var messageBox  = document.getElementById("display");
+
+function insert(){
+  var title = titleInput.value;
+  var name = nameInput.value;
+  var extension = extInput.value;
+
+	employees.push({
+  	title: title,
+    name: name,
+    extension: extension
+  });
+
+  clearAndShow();
+}
+
+function clearAndShow () {
+  // Clear fields
+  titleInput.value = "";
+  nameInput.value = "";
+  extInput.value = "";
+
+  var html = "";
+
+  // Show output
+  html += "Showing " + employees.length + " employees";
+  html += "<tr>";
+  html += "<td>Name</td>";
+  html += "<td>Title</td>";
+  html += "<td>Extension</td>";
+  html += "<td></td>";
+  html += "</tr>";
+  for(i = 0; i < employees.length; i++)
+  {
+    html += "<tr>";
+    html += "<td>" + employees[i].name + "</td>";
+    html += "<td>" + employees[i].title + "</td>";    
+    html += "<td>" + employees[i].extension + "</td>";
+    html += "<td>" + "<button type='button' id='delete'>Delete</button>" + "</td>";
+    html += "</tr>";
+  }
+  messageBox.innerHTML = html;
+  
+  //Delete Entry
 }
 
 
